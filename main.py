d4ef7e010f042ebec089aa5c45ecf9a28dbc5467
pietro24 <canevarolopietro@gmail.com>
2022-11-24 11:00:40 +0100

    result beautifier

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     terminalList = terminal.Terminal.getTerminalList()
 
     loadAddress = input("Please insert the loading Address: ")
-    unloadAddress = input("Please insert the loading Address: ")
+    unloadAddress = input("\nPlease insert the loading Address: ")
 
     geolocator = Nominatim(user_agent="Bridge_coding_challenge")
 
@@ -25,9 +25,25 @@ def main():
                          unloadingLocation.longitude)
     shorterUnloadTerminal = dist_calculator.calculateClosest(
         terminalList, points)
+    print("\n----------------------------------------------------------------------")
+    print("\nThe closest train terminals to " +
+          loadAddress + " is: "+shorterLoadTerminal.id)
 
-    print(shorterLoadTerminal.id)
-    print(shorterUnloadTerminal.id)
+    loadTerminalInfo = terminal.Terminal.getTerminalById(
+        shorterLoadTerminal.id)
+
+    print("\nSome terminal Info: " + loadTerminalInfo.address,
+          loadTerminalInfo.terminalOperator, loadTerminalInfo.phone, loadTerminalInfo.zipcode+"\n")
+    print("-------------------------------")
+    print("\nThe closest train terminals to " +
+          unloadAddress + " is: "+shorterUnloadTerminal.id)
+
+    unloadTerminalInfo = terminal.Terminal.getTerminalById(
+        shorterUnloadTerminal.id)
+
+    print("\nSome terminal Info: " + unloadTerminalInfo.address,
+          unloadTerminalInfo.terminalOperator, unloadTerminalInfo.phone, unloadTerminalInfo.zipcode+"\n")
+    print("----------------------------------------------------------------------")
 
 
 main()
--- models/terminal.py
+++ models/terminal.py
@@ -8,8 +8,19 @@ class Terminal:
     def __init__(self):
 
         self.id = ""
+        self.modesServed = ""
+        self.terminalOperator = ""
+        self.address = ""
+        self.phone = ""
+        self.web = ""
+        self.openingHours = ""
         self.lat = ""
         self.lng = ""
+        self.zipcode = ""
+        self.country = ""
+        self.region = ""
+        self.city = ""
+        self.state = ""
 
     def getTerminalList():
         terminals = Terminal.terminalsJSON
@@ -23,3 +34,25 @@ class Terminal:
             terminalsObjList.append(tempTerminal)
 
         return terminalsObjList
+
+    def getTerminalById(id):
+        terminal = list(filter(lambda x: x.get(
+            "terminal") == id, Terminal.terminalsJSON))
+
+        # check if terminal id exist
+        if len(terminal) < 1:
+            raise Exception("ID not found")
+
+        tempTerminal = Terminal()
+        tempTerminal.id = terminal[0]["terminal"]
+        tempTerminal.modesServed = terminal[0]["Modes Served"]
+        tempTerminal.terminalOperator = terminal[0]["Terminal Operator"]
+        tempTerminal.address = terminal[0]["Address"]
+        tempTerminal.phone = terminal[0]["Phone"]
+        tempTerminal.web = terminal[0]["Web"]
+        tempTerminal.lat = terminal[0]["address_info"]["lat"]
+        tempTerminal.lng = terminal[0]["address_info"]["lng"]
+        tempTerminal.zipcode = terminal[0]["address_info"]["zipcode"]
+        tempTerminal.country = terminal[0]["address_info"]["country"]
+
+        return tempTerminal
